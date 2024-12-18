674f7910e6a94a51dadc97220e786565be59720e
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-12-18 01:30:29 +0000

    Update message in index.js to test deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello, Node.js is running!");
+  res.send("Hello, Node.js app updated successfully via GitHub Actions!");
 });
 
 const port = 3000;
